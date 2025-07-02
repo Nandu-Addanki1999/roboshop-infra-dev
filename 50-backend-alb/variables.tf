2967604e5add9e7ccf2fb6858406dd660cf1a469
Nandu-Addanki1999 <nandua1999@gmail.com>
2025-07-02 22:38:08 +0530

    terraform

---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "ZZ09414452UFBO39K2SLBL"
+}
+
+variable "zone_name" {
+    default = "nandu.sbs"
+}
